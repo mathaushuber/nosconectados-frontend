c705295c103a99d3fc807f073292a97445ee3a9c
HuberM1998 <huber.mathaus@gmail.com>
2022-09-15 15:16:12 -0300

    Adicionando página inicial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,44 @@
 <template>
   <main>
-<div id="fundo">
-  <img src="../assets/fundo.jpg">
-</div>
-<div class="container">
- <div class="card center">
-       <div class="card-content">
-          <div class="content">
-            <img src="../assets/partamon.png">
-            <p>Soluções para o manejo integrado de pragas</p>
-            <h2 class="is-justify-content-left">Login</h2>
-             <b-input placeholder="E-mail"></b-input>
-             <b-input 
-             placeholder="Senha" 
-             class="mt-5"
-             type="password"
-             password-reveal></b-input><br>
-             <a href="" class="center">Esqueci minha senha</a><br>
-             <div class="buttons center is-justify-content-center">
-                                 <b-button 
-             type="is-success" 
-             tag="router-link"
-             :to="{ path: '/dashboard' }"
-             class="mt-5">Entrar</b-button>
-                                 <b-button
-                      class="mt-5"
-                      type="is-warning"
-                      tag="router-link"
-                      :to="{ path: '/registro' }"
-                      >
-                      Criar conta
-                    </b-button>
-                  </div>
-          </div>
-      </div>
+    <div id="fundo">
+      <img src="../assets/fundo.jpg">
     </div>
-  </div>  
+      <div class="container"><!-- Início container -->
+        <div class="card center"><!-- Início card-->
+          <div class="card-content"><!-- Início card-content -->
+            <div class="content"><!-- Início content -->
+
+                <img src="../assets/partamon.png">
+                <p>Soluções para o manejo integrado de pragas</p>
+                <h2 class="is-justify-content-center">Login</h2>
+                
+                 <b-input placeholder="E-mail"></b-input>
+                 <b-input 
+                 placeholder="Senha" 
+                 class="mt-5"
+                 type="password"
+                 password-reveal></b-input><br>
+                 <a href="" class="center">Esqueci minha senha</a><br>
+                 <div class="buttons center is-justify-content-center">
+                  <b-button 
+                     type="is-success" 
+                     tag="router-link"
+                     :to="{ path: '/dashboard' }"
+                     class="mt-5">Entrar
+                  </b-button>                   
+                  <b-button
+                     class="mt-5"
+                     type="is-warning"
+                     tag="router-link"
+                     :to="{ path: '/registro' }">
+                     Criar conta
+                  </b-button>
+                 </div>
+
+            </div><!-- Fim content -->
+          </div><!-- Fim card-content-->
+        </div><!-- Fim content-->
+      </div><!-- Fim container -->  
   </main>
 </template>
 
