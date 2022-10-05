5288f7e71149a4f66e1c78cf6cedd6353aff0985
HuberM1998 <huber.mathaus@gmail.com>
2022-10-05 12:57:56 -0300

    adicionando vue leaflet à aplicação

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import Buefy from "buefy";
 import router from "./router";
 import VueMask from "v-mask";
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
 // import "buefy/dist/buefy.css";
 import "./styles/style.scss";
 import "@mdi/font/css/materialdesignicons.css";
@@ -10,6 +12,9 @@ import "@fortawesome/fontawesome-free/css/all.css";
 Vue.use(Buefy);
 Vue.config.productionTip = false;
 Vue.use(VueMask);
+Vue.component('l-map', LMap);
+Vue.component('l-tile-layer', LTileLayer);
+Vue.component('l-marker', LMarker);
 
 new Vue({
   router,
