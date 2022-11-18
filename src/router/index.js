5d87bc0476b8f46b6f9330f37ce8fb7331b10e71
HuberM1998 <huber.mathaus@gmail.com>
2022-11-18 20:07:45 -0300

    Modificações

--- src/router/index.js
+++ src/router/index.js
@@ -22,6 +22,12 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/RegisterUser.vue"),
   },
+  {
+    path: "/create-sensor",
+    name: "CadastroSensor",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/CreateSensor.vue"),
+  },
 ];
 
 const router = new VueRouter({
