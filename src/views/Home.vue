58eb160f5b291eccf22d92326340584a72bd9781
Mathaus Huber <huber.mathaus@gmail.com>
2022-12-29 18:29:39 -0300

    Corrigindo link "esqueci minha senha"

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,7 +18,7 @@
                  class="mt-5"
                  type="password"
                  password-reveal></b-input><br>
-                 <a href="" class="center">Esqueci minha senha</a><br>
+                 <a class="center" @click="showAlert()">Esqueci minha senha</a><br>
                  <div class="buttons center is-justify-content-center">
                   <b-button 
                      type="is-success" 
@@ -42,6 +42,48 @@
   </main>
 </template>
 
+<script>
+export default{
+  methods: {
+    showAlert(){
+      this.$swal({
+  title: 'Entre com o seu e-mail',
+  subtitle: 'Enviaremos uma solicitação de nova senha por lá!',
+  input: 'text',
+  inputAttributes: {
+    autocapitalize: 'off'
+  },
+  showCancelButton: true,
+  confirmButtonText: 'Enviar',
+  cancelButtonText: 'Cancelar',
+  showLoaderOnConfirm: true,
+  preConfirm: (login) => {
+    return fetch(`https://nosconectados.herokuapp.com/api/users?rq=read${login}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.statusText)
+        }
+        return response.json()
+      })
+      .catch(error => {
+        this.$swal.showValidationMessage(
+          `Request failed: ${error}`
+        )
+      })
+  },
+  allowOutsideClick: () => !this.$swal.isLoading()
+}).then((result) => {
+  if (result.isConfirmed) {
+    this.$swal({
+      title: `${result.value.login}'s avatar`,
+      imageUrl: result.value.avatar_url
+    })
+  }
+})
+    },
+  },
+};
+</script>
 <style lang="scss" scoped>
 @import url("https://fonts.googleapis.com/css2?family=Staatliches&display=swap");
 main{
@@ -61,7 +103,7 @@ main{
   padding: 30px;
   }
 }
-  .center {
+.center {
   margin-left: auto;
   margin-right: auto;
   text-align: center;
