71b0df88d69ee368523a223735853f5ee3d033a6
Mathaus Huber <huber.mathaus@gmail.com>
2023-01-11 15:39:24 -0300

    alterando layout

--- src/views/Sensores.vue
+++ src/views/Sensores.vue
@@ -7,7 +7,7 @@
                         <img src="../assets/sensorespublicos.svg" width="250" height="130">
                     </div>
                     <h1 class="is-size-6 is-uppercase has-text-weight-light pb-2 mt-4 center">
-                        AQUI VOCÊ PODE FILTRAR PELOS SENSORES DE DESEJADOS.
+                        AQUI VOCÊ PODE FILTRAR PELOS SENSORES DESEJADOS.
                     </h1>
                     <div class="mt-5">
                         <b-field label="Propriedade">
