4a571754ea51476ba96797e74c3891e5ce4bb600
Mathaus Huber <huber.mathaus@gmail.com>
2023-04-21 16:35:01 -0300

    corrigindo endereço da api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
   const API_URL = process.env.VUE_APP_API_URL;
   const api = axios.create({
-    baseURL: `${API_URL}api`,
+    baseURL: `${API_URL}/api`,
   });
 
   function loginUser(email, user_password) {
